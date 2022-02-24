82ffd068f3f91a1dbb084eb3eaae0fd18aa55571
Jack <jack.bowling@omron.com>
2022-02-24 11:43:36 -0500

    Add project files.

---
+++ LVS_ImageTransfer/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using ControlzEx.Theming;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LVS_ImageTransfer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Databases.SimpleDatabase Settings { get; private set; }
+
+#if DEBUG
+        public static string SettingsFileRootDir { get; set; } = System.IO.Directory.GetCurrentDirectory();
+#else        
+        public static string SettingsFileRootDir { get; set; } = $"{Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData)}\\TDD\\LVS_ImageTransfer\\";
+#endif
+
+        public static string UserDataDirectory => $"{SettingsFileRootDir}\\UserData";
+        public static string MapDatabaseSettingsFile => "ApplicationSettings";
+        public static string MapDatabaseExtension => ".sqlite";
+
+        public App()
+        {
+
+            if (!Directory.Exists(UserDataDirectory))
+            {
+                _ = Directory.CreateDirectory(UserDataDirectory);
+            }
+
+            Settings = new Databases.SimpleDatabase().Init(Path.Combine(UserDataDirectory, $"{MapDatabaseSettingsFile}{MapDatabaseExtension}"), false);
+
+            if (Settings == null)
+            {
+               return;
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            // Set the application theme to Dark.Green
+            _ = ThemeManager.Current.ChangeTheme(this, Settings.GetValue("App.Theme", "Dark.Steel"));
+
+            ThemeManager.Current.ThemeChanged += Current_ThemeChanged;
+            ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncWithAppMode;
+        }
+        private void Current_ThemeChanged(object sender, ThemeChangedEventArgs e) => App.Settings.SetValue("App.Theme", e.NewTheme.Name);
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+
+            Settings.Dispose();
+        }
+    }
+}
---
+++ LVS_ImageTransfer/WindowViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LVS_ImageTransfer.WindowViewModele
+{
+    public class MainWindowViewModel:Core.BaseViewModel
+    {
+        public string ImageSourcePath { get => App.Settings.GetValue("ImageSourcePath", ""); set => App.Settings.SetValue("ImageSourcePath", value);  }
+
+        public string FTPServerIP { get => App.Settings.GetValue("FTPServerIP", ""); set => App.Settings.SetValue("FTPServerIP", value); }
+        public string FTPServerPath { get => App.Settings.GetValue("FTPServerPath", ""); set => App.Settings.SetValue("FTPServerPath", value); }
+        public string FTPServerUserName { get => App.Settings.GetValue("FTPServerUserName", ""); set => App.Settings.SetValue("FTPServerUserName", value); }
+        public string FTPServerPassword { get => App.Settings.GetValue("FTPServerPassword", ""); set => App.Settings.SetValue("FTPServerPassword", value); }
+
+    }
+}
