d5eee9ea54d7b56c96211edd4d519a2aac57894e
Jack <jack.bowling@omron.com>
2022-02-25 12:18:18 -0500

    alpha functional

---
+++ LVS_ImageTransfer/Controllers/PNGProcess.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace LVS_ImageTransfer.Controllers
+{
+    public class PNGProcess
+    {
+        public BitmapSource GetBitmapOf(string path)
+        {
+            Stream imageStreamSource = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read);
+            PngBitmapDecoder decoder = new PngBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
+            return decoder.Frames[0];
+        }
+
+        public void Resize(string imageFile, string outputFile, double scaleFactor)
+        {
+            using (var srcImage = Image.FromFile(imageFile))
+            {
+                var newWidth = (int)(srcImage.Width * scaleFactor);
+                var newHeight = (int)(srcImage.Height * scaleFactor);
+                using (var newImage = new Bitmap(newWidth, newHeight))
+                using (var graphics = Graphics.FromImage(newImage))
+                {
+                    graphics.SmoothingMode = SmoothingMode.HighQuality;
+                    graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                    
+                    graphics.DrawImage(srcImage, new Rectangle(0, 0, newWidth, newHeight));
+                    newImage.Save(outputFile, ImageFormat.Jpeg);
+                }
+            }
+        }
+
+    }
+}
--- LVS_ImageTransfer/WindowViewModels/MainWindowViewModel.cs
+++ LVS_ImageTransfer/WindowViewModels/MainWindowViewModel.cs
@@ -1,19 +1,86 @@
-﻿using System;
+﻿using LVS_ImageTransfer.Controllers;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace LVS_ImageTransfer.WindowViewModele
 {
-    public class MainWindowViewModel:Core.BaseViewModel
+    public class MainWindowViewModel : Core.BaseViewModel
     {
-        public string ImageSourcePath { get => App.Settings.GetValue("ImageSourcePath", ""); set => App.Settings.SetValue("ImageSourcePath", value);  }
+        public bool Started
+        {
+            get => started;
+            set { SetProperty(ref started, value); OnPropertyChanged("NotStarted"); }
+        }
+        private bool started = false;
 
-        public string FTPServerIP { get => App.Settings.GetValue("FTPServerIP", ""); set => App.Settings.SetValue("FTPServerIP", value); }
-        public string FTPServerPath { get => App.Settings.GetValue("FTPServerPath", ""); set => App.Settings.SetValue("FTPServerPath", value); }
-        public string FTPServerUserName { get => App.Settings.GetValue("FTPServerUserName", ""); set => App.Settings.SetValue("FTPServerUserName", value); }
-        public string FTPServerPassword { get => App.Settings.GetValue("FTPServerPassword", ""); set => App.Settings.SetValue("FTPServerPassword", value); }
+        public bool NotStarted => !started;
+
+        public string ImageSourcePath { get => App.Settings.GetValue("ImageSourcePath", ""); set => App.Settings.SetValue("ImageSourcePath", value); }
+        public string Host { get => App.Settings.GetValue("FTPHost", ""); set => App.Settings.SetValue("FTPHost", value); }
+        public string RootPath { get => App.Settings.GetValue("FTPRootPath", "/"); set => App.Settings.SetValue("FTPRootPath", value); }
+       //public string Path { get => App.Settings.GetValue("FTPPath", ""); set => App.Settings.SetValue("FTPPath", value); }
+        public string UserName { get => App.Settings.GetValue("FTPUserName", ""); set => App.Settings.SetValue("FTPUserName", value); }
+        public string Password { get => App.Settings.GetValue("FTPPassword", ""); set => App.Settings.SetValue("FTPPassword", value); }
+
+        public ICommand Start { get; }
+        public ICommand Stop { get; }
+
+        private FTPController FTPController { get; } = new FTPController();
+        private FolderWatcher FolderWatcher { get; } = new FolderWatcher();
+
+        public MainWindowViewModel()
+        {
+            Start = new Core.RelayCommand(StartAction, c => true);
+            Stop = new Core.RelayCommand(StopAction, c => true);
+        }
+
+        private void StartAction(object parameter)
+        {
+            if (Directory.Exists(ImageSourcePath))
+            {
+                Started = true;
+                FolderWatcher.Start(ImageSourcePath);
+
+                FolderWatcher.OnNewImage -= FolderWatcher_OnNewImage;
+                FolderWatcher.OnNewImage += FolderWatcher_OnNewImage;
+            }
+        }
+
+        private void FolderWatcher_OnNewImage(string path)
+        {
+            Thread.Sleep(100);
+
+            string ext = Path.GetExtension(path);
+            string name = Path.GetFileName(path).Replace(ext, string.Empty);
+            
+            string dir = Path.GetDirectoryName(path);
+            string newFile = $"{App.SettingsFileRootDir}\\Temp\\{name}.jpg";
+
+            Directory.CreateDirectory($"{App.SettingsFileRootDir}\\Temp\\");
+
+            new PNGProcess().Resize(path, newFile, 0.8);
+
+            FTPController.Host = Host;
+            FTPController.RootPath = RootPath;
+            FTPController.UserName = UserName;
+            FTPController.Password = Password;
+
+            FTPController.OpenWrite(newFile, Path.GetFileName(newFile));
+
+            File.Delete(newFile);
+        }
+
+        private void StopAction(object parameter)
+        {
+            Started = false;
+            FolderWatcher.Stop();
+        }
 
     }
 }
