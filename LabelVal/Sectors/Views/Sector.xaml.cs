da3ea6d3a8d4dc4f3f40ef8e202a390b4fb1560d
Jack Bowling <jack@jackb.me>
2024-06-27 14:23:05 -0400

    Refactor and enhance Sector UI interactions
    - In Sector.xaml, added a `Loaded` event handler and introduced new converters for UI data binding, enhancing the user experience by dynamically adjusting visibility, string formatting, and trimming based on bound data.
    - In Sector.xaml.cs, streamlined imports and removed unused directives, improving code readability and performance. Removed the summary comment for the `Sector` class, possibly indicating a shift towards external documentation. Added new properties `Vm` and `GroupName` to interact with the view model and manage UI focus logic. Simplified the constructor and refactored the method for showing sector details to dynamically set properties based on the context. Introduced event handlers for new UI functionalities and added logic to display details of sectors with the same name and group, significantly enhancing user interaction.
    - Updated ToDo.txt to reflect completed tasks related to UI improvements, including sector coloring, display of details panels, and clicking behavior on sector details, indicating progress in the project's UI development.

--- LabelVal/Sectors/Views/Sector.xaml.cs
+++ LabelVal/Sectors/Views/Sector.xaml.cs
@@ -1,70 +1,111 @@
-﻿using System.Windows;
+﻿using LabelVal.Results.Views;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace LabelVal.Sectors.Views;
 
-/// <summary>
-/// Interaction logic for SectorControlView.xaml
-/// </summary>
 public partial class Sector : UserControl
 {
+    private Results.ViewModels.ImageResultEntry Vm { get; set; }
     public string SectorName => ((ViewModels.Sector)DataContext).Template.Username;
+    public string GroupName { get; private set; }
+
+    public bool IsSectorFocused => GroupName switch
+    {
+        "v275Stored" => Vm.V275FocusedStoredSector != null,
+        "v275Current" => Vm.V275FocusedCurrentSector != null,
+        "v5Stored" => Vm.V5FocusedStoredSector != null,
+        "v5Current" => Vm.V5FocusedCurrentSector != null,
+        "l95xxStored" => Vm.L95xxFocusedStoredSector != null,
+        "l95xxCurrent" => Vm.L95xxFocusedCurrentSector != null,
+        _ => false,
+    };
+
     public Sector()
     {
         InitializeComponent();
     }
 
-    private void btnGS1DecodeText_Click(object sender, RoutedEventArgs e) => popGS1DecodeText.IsOpen = true;
-    private void btnOverallGrade_Click(object sender, RoutedEventArgs e) => ShowSectorDetails();
-
-    public void ShowSectorDetails()
+    private void UserControl_Loaded(object sender, RoutedEventArgs e)
     {
-        Results.ViewModels.ImageResultEntry vm = null;
-        string str = string.Empty;
+        GetSecotorDetails();
+    }
+
+    private void GetSecotorDetails()
+    {
         var list = Utilities.VisualTreeHelp.GetVisualParent<ListView>(this);
         if (list != null)
         {
-            str = list.Tag.ToString();
-            vm = (Results.ViewModels.ImageResultEntry)list.DataContext;
+            GroupName = list.Tag.ToString();
+            Vm = (Results.ViewModels.ImageResultEntry)list.DataContext;
         }
         else
         {
             var itmc = Utilities.VisualTreeHelp.GetVisualParent<ItemsControl>(this);
             if (itmc != null)
             {
-                str = itmc.Tag.ToString();
-                vm = (Results.ViewModels.ImageResultEntry)itmc.DataContext;
+                GroupName = itmc.Tag.ToString();
+                Vm = (Results.ViewModels.ImageResultEntry)itmc.DataContext;
             }
         }
+    }
 
-        if (vm == null)
+    private void btnGS1DecodeText_Click(object sender, RoutedEventArgs e)
+    {
+        popGS1DecodeText.IsOpen = true;
+    }
+
+    private void btnOverallGrade_Click(object sender, RoutedEventArgs e)
+    {
+        ShowSectorDetails();
+    }
+
+    public void ShowSectorDetails()
+    {
+        if (Vm == null)
             return;
 
-        switch (str)
+        //This lets us know which sector is being focused on
+        switch (GroupName)
         {
             case "v275Stored":
-                vm.V275FocusedStoredSector = (ViewModels.Sector)this.DataContext;
+                Vm.V275FocusedStoredSector = (ViewModels.Sector)this.DataContext;
                 break;
             case "v275Current":
-                vm.V275FocusedCurrentSector = (ViewModels.Sector)this.DataContext;
+                Vm.V275FocusedCurrentSector = (ViewModels.Sector)this.DataContext;
+                ShowSameNameSector("v275Stored");
                 break;
             case "v5Stored":
-                vm.V5FocusedStoredSector = (ViewModels.Sector)this.DataContext;
+                Vm.V5FocusedStoredSector = (ViewModels.Sector)this.DataContext;
                 break;
             case "v5Current":
-                vm.V5FocusedCurrentSector = (ViewModels.Sector)this.DataContext;
+                Vm.V5FocusedCurrentSector = (ViewModels.Sector)this.DataContext;
                 break;
             case "l95xxStored":
-                vm.L95xxFocusedStoredSector = (ViewModels.Sector)this.DataContext;
+                Vm.L95xxFocusedStoredSector = (ViewModels.Sector)this.DataContext;
                 break;
             case "l95xxCurrent":
-                vm.L95xxFocusedCurrentSector = (ViewModels.Sector)this.DataContext;
+                Vm.L95xxFocusedCurrentSector = (ViewModels.Sector)this.DataContext;
                 break;
         }
     }
 
+    private void ShowSameNameSector(string targetGroup)
+    {
+        var ire = Utilities.VisualTreeHelp.GetVisualParent<ImageResultEntry_V275>(this);
+        if (ire != null)
+        {
+            var sectors = Utilities.VisualTreeHelp.GetVisualChildren<Sector>(ire);
+            foreach (var s in sectors)
+            {
+                if (s.GroupName == targetGroup && s.SectorName == SectorName)
+                {
+                    if (!s.IsSectorFocused)
+                        s.ShowSectorDetails();
+                    break;
+                }
+            }
+        }
+    }
 
 }
