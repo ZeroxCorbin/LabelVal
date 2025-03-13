5e206785398507cb463db3cf9471a01f9d438851
Jack <jack@jackb.me>
2025-03-12 22:23:00 -0400

    Initialize Suffix and update region type bindings
    - Set the `Suffix` property in `ValuePassFail.cs` to an empty string.
    - Added `ParameterSuffixToUnitsMultiConverter` for better unit formatting.
    - Changed bindings in `Sector.xaml` from `Report.Type` to `Report.RegionType`.
    - Updated `SectorDetails.xaml` to utilize the new converter for displaying units.
    - Adjusted visibility and text properties to improve UI responsiveness.
    - Refined XAML structure for consistency and maintainability.

---
+++ LabelVal/Sectors/Converters/ParameterSuffixToUnitsMultiConverter.cs
@@ -0,0 +1,16 @@
+﻿using BarcodeVerification.lib.Common;
+using System.Windows.Data;
+
+namespace LabelVal.Sectors.Converters;
+
+public class ParameterSuffixToUnitsMultiConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+    {
+        return (values[0] is not string s) || (values[1] is not AvailableUnits units)
+            ? values[0]
+            : s.Equals("<units>") ? units.GetDescription() : s;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture) => throw new NotImplementedException();
+}
