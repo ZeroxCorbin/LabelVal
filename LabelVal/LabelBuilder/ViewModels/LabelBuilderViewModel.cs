79a62e62f647941145a1e7d8942b3b0eaa59ac5c
Jack <jack.bowling@omron.com>
2025-09-16 15:56:39 -0400

    Add new features and enhancements to barcode builder
    - Updated `BarcodeBuilder.lib.Wpf.csproj` to include `WPF.JoshSmith.Controls.DragCanvas` version `2.0.3`.
    - Introduced `FrameworkElement_ClickProcessStart` for handling mouse click events.
    - Added `NewBarcodeEntity` class to encapsulate barcode entities.
    - Modified `BarcodeBuilderViewModel` to include `DisplayEditorViewModel`.
    - Expanded `BarcodeEntity` with new properties and methods for image handling.
    - Updated `DisplayEditorViewModel` to manage barcode and display entities.
    - Enhanced XAML for `DisplayEditorView` with new styles and data bindings.
    - Updated code-behind for context menu actions in `DisplayEditorView`.
    - Adjusted `LabelBuilderViewModel` to initialize with new settings structure.
    - Modified `LabelBuilderView.xaml` for proper data context bindings.

--- LabelVal/LabelBuilder/ViewModels/LabelBuilderViewModel.cs
+++ LabelVal/LabelBuilder/ViewModels/LabelBuilderViewModel.cs
@@ -1,12 +1,13 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using BarcodeBuilder.lib.Wpf.ViewModels;
+using CommunityToolkit.Mvvm.ComponentModel;
 using LabelVal.Main.ViewModels;
 
 namespace LabelVal.LabelBuilder.ViewModels;
 
 public class LabelBuilderViewModel : ObservableObject
 {
-    public BarcodeBuilder.lib.Wpf.ViewModels.BarcodeBuilderViewModel BarcodeBuilderViewModel { get; } = new BarcodeBuilder.lib.Wpf.ViewModels.BarcodeBuilderViewModel(App.Settings);
-    public DisplayEditorViewModel DisplayEditorViewModel { get; } = new DisplayEditorViewModel();
+    public BarcodeBuilder.lib.Wpf.ViewModels.BarcodeBuilderViewModel BarcodeBuilderViewModel { get; } = new BarcodeBuilder.lib.Wpf.ViewModels.BarcodeBuilderViewModel(App.DisplayDatabase, App.Settings);
+
 
     public GlobalAppSettings AppSettings => GlobalAppSettings.Instance;
 }
